95532a5aaf6875e20f257d12e0065f47b96d0e12
rohan-kumrawat <rohanrkmail@gmail.com>
2024-11-30 19:02:34 +0530

    Add real-time notifications for application status updates

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -1,5 +1,6 @@
 const Application = require('../models/Application');
 const Job = require('../models/Job');
+const io = require('../app');
 
 exports.applyToJob = async (req, res) => {
   const { jobId } = req.params;
@@ -53,7 +54,7 @@ exports.getUserApplications = async (req, res) => {
 
 exports.updateApplicationStatus = async (req, res) => {
   const { applicationId } = req.params;
-  const { status } = req.body; // New status
+  const { status } = req.body;
 
   try {
     const application = await Application.findById(applicationId);
@@ -61,8 +62,17 @@ exports.updateApplicationStatus = async (req, res) => {
       return res.status(404).json({ message: 'Application not found' });
     }
 
+    // Update the application status
     application.status = status;
     await application.save();
+
+    // Emit notification to the user
+    const notification = {
+      userId: application.user,
+      message: `Your application status for job "${application.job.title}" has been updated to: ${status}`,
+    };
+
+    io.emit('application-status-update', notification); // Emit to the user via Socket.io
 
     res.status(200).json({ message: 'Application status updated', application });
   } catch (error) {
