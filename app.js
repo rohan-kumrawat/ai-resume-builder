18c9daabf519c7bcc7a81d424afdbbf97d41a534
rohan-kumrawat <rohanrkmail@gmail.com>
2024-12-04 17:00:16 +0530

    refactor: moved MongoDB connection logic to config file

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5000;
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes");
 const resumeRoutes = require("./routes/resumeRoutes");
 const jobRoutes = require("./routes/jobRoutes");
 
 dotenv.config();
+connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -15,11 +17,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/resumes", resumeRoutes);
 app.use("/api/jobs", jobRoutes);
 
-const PORT = process.env.PORT || 5000;
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
 
 app.get("/", (req, res) => res.send("API is running..."));
 
